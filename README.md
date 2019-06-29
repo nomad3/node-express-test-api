325d70811c9f83b04d9a775148ee27ef5d7a7509
Simon Aguilera <saguilera1608@gmail.com>
2019-06-29 18:46:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# mongo-crud
+
+[![Build Status](https://travis-ci.org/nomad3/weknow-test-api.svg?branch=master)](https://travis-ci.org/nomad3/weknow-test-api)
+
+A CRUD API built with MongoDB and Express.
+
+# Build
+
+`npm run build`
+
+# Run
+
+`npm run production`
+
+# Test
+
+`npm test`
---
+++ mongo.query.js
@@ -0,0 +1,91 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const { inspect } = require('util');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'fatvStats';
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  const db = client.db(dbName);
+  const conversation_turns = db.collection('conversation_turns');
+  conversation_turns.createIndex({ 'entities.start_time': -1 })
+    .then(() => conversation_turns.aggregate([
+      // UNWINDING entities
+      { $unwind: {
+          path: '$entities',
+          preserveNullAndEmptyArrays: true,
+      } },
+      // ONLY THOSE entities IN FEBURARY
+      { $match: {
+        $or: [
+          { 'entities': undefined, }, // CASE NO entities
+          { $and: [
+              { 'entities.start_time': { $gte: new Date('Feburary 1, 2018 00:00:00') } },
+              { 'entities.start_time': { $lt: new Date('March 1, 2018 00:00:00') } },
+            ],
+          },
+        ],
+      } },
+      // UNWINDING topic value
+      { $unwind: {
+          path: '$entities.value',
+          preserveNullAndEmptyArrays: true,
+      } },
+      // REASSEMBLE topic value WITH FIRST item ONLY
+      { $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          count: {$sum: 1},
+          entities: {
+            _id: '$entities._id',
+            start_time: '$entities.start_time',
+          },
+        },
+        value: {
+          $first: '$entities.value',
+        },
+      } },
+      // FIX EMPTY value
+      { $project: {
+        value: {
+          $cond: {
+            if: {
+              $eq: [ '$value', null ],
+            },
+            then: [],
+            else: [ '$value' ],
+          },
+        },
+      } }, 
+      // REASSEMBLE entities
+      { $group: {
+        _id: '$_id._id',
+        name: { $first: '$_id.name' },
+        entities: {
+          $push: {
+            _id: '$_id.entities._id',
+            start_time: '$_id.entities.start_time',
+            value: '$value',
+          },
+        },
+      } },
+      // FIX EMPTY entities
+      { $project: {
+        name: true,
+        entities: {
+          $cond: {
+            if: {
+              $eq: [ '$entities', [{ value: [] }] ],
+            },
+            then: [],
+            else: '$entities',
+          },
+        },
+      } }, 
+    ]).toArray())
+    .then(results => {
+      console.log(inspect(results, false, null));
+    })
+    .then(() => client.close());
+});
